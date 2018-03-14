25c8bdfb2d47c1164fc80b6977137c725be640d3
Krocsky <1164994684@qq.com>
2018-03-14 22:47:30 +0800

    community invitation

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 import Express from 'express'
 import home from './home'
 import notFound from './404'
+import invite from './invite'
 
 const router = Express.Router()
 
 router.get(['/', '/wl', '/pwl', '/pt*'], home)
+router.get(['/', '/invite'], invite)
 router.get(/^(?!.*(webpack|\.))/ig, notFound)
 
 export default router
---
+++ server/routes/invite.js
@@ -0,0 +1,15 @@
+import { home as mainFilePath } from '../../HashMapping.json'
+import config from '../../config/env'
+
+export default function (req, res) {
+
+  let renderData = {
+    title: '活动邀请',
+    main: config.globals.__DEV__ ? 'invite.js' : mainFilePath,
+    apiServer: config.api_server,
+    DEV: config.globals.__DEV__,
+  }
+
+  res.set('content-type', 'text/html')
+  res.render('invite', renderData)
+}
