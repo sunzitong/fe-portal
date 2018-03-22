67e5803831e7f6dcc8ff2a4757826eda4ff84ad4
Krocsky <1164994684@qq.com>
2018-03-22 18:24:16 +0800

    activity complete

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,13 @@ import Express from 'express'
 import home from './home'
 import notFound from './404'
 import invite from './invite'
+import inviteRule from './inviteRule'
 
 const router = Express.Router()
 
 router.get(['/', '/wl', '/pwl', '/pt*'], home)
 router.get(['/invite'], invite)
+router.get(['/inviteRule'], inviteRule)
 router.get(/^(?!.*(webpack|\.))/ig, notFound)
 
 export default router
